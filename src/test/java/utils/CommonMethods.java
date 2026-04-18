32102f687b4f6d8101e8a2489b47561e2e5b671d
Nissrine-Can <niss.canina@gmail.com>
2026-04-18 14:48:19 -0400

    added headless browser

--- src/test/java/utils/CommonMethods.java
+++ src/test/java/utils/CommonMethods.java
@@ -3,6 +3,7 @@ package utils;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
@@ -25,8 +26,8 @@ public class CommonMethods extends PageInitializer{
         switch (ConfigReader.read("browser")){
 
             case "Chrome":
-                //ChromeOptions options = new ChromeOptions();
-                // options.addArguments("--headless");
+                ChromeOptions options = new ChromeOptions();
+                 options.addArguments("--headless");
                 driver=new ChromeDriver();
                 break;
             case "FireFox":
